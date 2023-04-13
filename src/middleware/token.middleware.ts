667b0005115e7bb36f06dcc6bb8f4eaf7905d062
enzoleao <enzopinheiro6@gmail.com>
2023-04-13 09:38:39 -0300

    update

--- src/middleware/token.middleware.ts
+++ src/middleware/token.middleware.ts
@@ -4,10 +4,6 @@ import { verify } from 'jsonwebtoken'
 
 export const tokenAuthentication = ( req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split('Bearer ')[1]
-
-    if (!token){
-        res.status(401).json({message: 'token not provided'})
-    }
     try {
         verify(`${token}`, process.env.SECRET as string)
         next()
